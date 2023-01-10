a5862c963922a1910e22fd5e0d680cd866e71dee
technicalprojects-pooja <09102000pg@gmail.com>
2023-01-10 14:51:09 +0900

    first commit

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim result As Integer = Int32.Parse(Text1.Text) + Int32.Parse(Text2.Text)
+        tb.Text = result.ToString()
+    End Sub
+
+
+End Class
